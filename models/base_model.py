9b527cb114c7f46f5566c7b4db59b0b22f9060d3
Kefikhalil <naroqt@gmail.com>
2020-06-30 04:26:48 -0700

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -5,9 +5,44 @@ import models
 from datetime import datetime
 
 
-Class BaseModel:
-""" xxx """
-def __str__(self):
-    """returns a string of class name, id, and dictionary"""
-    return "[{}] ({}) {}".format(
-        type(self).__name__, self.id, self.__dict__)
+class BaseModel():
+    """ class base model """
+    def __init__(self, *args, **kwargs):
+        """Instantiation of base model class
+        Args : won't be used.
+        Attributes:
+            id: unique id generated
+            created_at: creation date
+            updated_at: updated date
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """returns a string of class name, id, and dictionary"""
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        '''
+            Update the updated_at attribute with new.
+        '''
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """creates dictionary of the class
+        Return:
+            return a dictionary of all the key values in __dict__
+        """
+        my_dict = dict(self.__dict__)
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        return my_dict
