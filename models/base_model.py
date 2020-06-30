38de2a3522dad6a1480b823c1e4fa922aa53c401
Kefikhalil <naroqt@gmail.com>
2020-06-30 08:41:09 -0700

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 import uuid
 import models
 from datetime import datetime
+from models import storage
 
 
 class BaseModel():
@@ -24,6 +25,7 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """returns a string of class name, id, and dictionary"""
@@ -35,6 +37,7 @@ class BaseModel():
             Update the updated_at attribute with new.
         '''
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """creates dictionary of the class
---
+++ models/engine/file_storage.py
@@ -0,0 +1,49 @@
+from models.base_model import BaseModel
+import json
+from os import path
+''' FileStorage module'''
+
+
+class FileStorage():
+    ''' serializes instances to a JSON file
+    and deserializes JSON file to instances
+    '''
+    __file_path = "file.json"
+    __objects = {}
+
+    instances = {
+        "BaseModel": BaseModel
+    }
+
+    def all(self):
+        '''returns the dictionary __objects'''
+        return FileStorage.__objects
+
+    def new(self, obj):
+        '''sets in __objects the obj with key <obj class name>.id'''
+        my_id = obj.id
+        my_cls = obj.__class__.__name__
+        my_key = my_cls + "." + my_id
+        FileStorage.__objects[my_key] = obj
+
+    def save(self):
+        '''serializes __objects to the JSON file (path: __file_path)'''
+        my_dict = {}
+        with open(FileStorage.__file_path, "w") as ff:
+            for key, value in FileStorage.__objects.items():
+                my_dict[key] = value.to_dict()
+            json.dump(my_dict, ff)
+
+    def reload(self):
+        '''deserializes the JSON file to __objects (only if the JSON file
+        (__file_path) exists ; otherwise, do nothing.
+         If the file doesn’t exist, no exception should be raised)'''
+        if path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r") as xk:
+                o = json.load(xk)
+                mydict = {}
+                for key, value in obj.items():
+                    mydict[key] = self.instances[value["__class__"]](**value)
+                FileStorage.__objects = mydict
+        else:
+            return
