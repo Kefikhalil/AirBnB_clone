89165830406b7d2a8c34c433baf54991140f248a
Kefikhalil <naroqt@gmail.com>
2020-07-18 16:26:37 +0000

    Update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,19 @@
 '''TestBaseModels module'''
 import unittest
+from models.base_model import BaseModel
 
 
 class TestBaseModels(unittest.TestCase):
     '''
     Test BAse Models class
     '''
+    def test_save(self):
+        base1 = BaseModel()
+        beforeCreatedAt = base1.created_at
+        beforeUpdatedAt = base1.updated_at
+        base1.save()
+        afterCreatedAt = base1.created_at
+        afterUpdatedAt = base1.updated_at
+        self.assertEqual(beforeCreatedAt, afterCreatedAt)
+        self.assertNotEqual(afterCreatedAt, afterUpdatedAt)
     pass
